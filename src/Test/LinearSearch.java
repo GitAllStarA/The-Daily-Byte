34848a60c17575c33643b9f823c0ba0ab0236ec0
avr <avr2820@gmail.com>
2023-06-29 01:37:20 -0500

    Recursion - String patterns

--- src/Test/LinearSearch.java
+++ src/Test/LinearSearch.java
@@ -1,5 +1,19 @@
 package Test;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int arr[] = {10, 50, 30, 70, 80, 20, 90, 40};
+        int key = 130;
+        System.out.println(linearSearch(arr,key,0));
+    }
 
+    static boolean linearSearch(int[] array, int target, int index){
+        if (index == array.length){
+            return false;
+        }
+        if (array[index] == target){
+            return true;
+        }
+        return linearSearch(array,target,index+1);
+    }
 }
