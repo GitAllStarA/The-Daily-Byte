da097d5e78e389c3743fcecdff0285feedbd93ad
avr <avr2820@gmail.com>
2023-07-03 22:09:31 -0700

    push from CA

--- src/Test/LinearSearch.java
+++ src/Test/LinearSearch.java
@@ -1,10 +1,15 @@
 package Test;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int arr[] = {10, 50, 30, 70, 80, 20, 90, 40};
-        int key = 130;
-        System.out.println(linearSearch(arr,key,0));
+        int arr[] = {10, 50, 30, 70, 80, 20, 90, 40, 40};
+        int key = 40;
+        //System.out.println(linearSearch(arr,key,0));
+        linearSearchMultipleOccurance(arr,key,0);
+        System.out.println(list);
+        System.out.println(linearSearchMultipleOccurance(arr,40,0,new ArrayList<>()));
     }
 
     static boolean linearSearch(int[] array, int target, int index){
@@ -15,5 +20,37 @@ public class LinearSearch {
             return true;
         }
         return linearSearch(array,target,index+1);
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void linearSearchMultipleOccurance(int[] array, int target, int index) {
+        if (index == array.length){
+            return;
+        }
+
+        if (array[index] == target){
+            list.add(index);
+
+
+        }
+         linearSearchMultipleOccurance(array,target,index+1);
+
+
+    }
+
+
+    static ArrayList<Integer> linearSearchMultipleOccurance(int[] array, int target, int index, ArrayList<Integer> returnList) {
+        if (index == array.length){
+            return returnList;
+        }
+
+        if (array[index] == target){
+
+            returnList.add(index);
+
+        }
+        return linearSearchMultipleOccurance(array,target,index+1,returnList);
+
+
     }
 }
